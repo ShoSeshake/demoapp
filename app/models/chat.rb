ac0d9d390484c4014a154026033cbb94df833311
ShoSeshake <sho122seshake@gmail.com>
2019-11-28 00:16:52 +0900

    model updated

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,2 +1,7 @@
 class Chat < ApplicationRecord
+  belongs_to :adviser, class_name: "User"
+  belongs_to :user, class_name: "User"
+  has_many :chat_reviews
+
+  validates :start_at presence: true
 end
--- app/models/chat_review.rb
+++ app/models/chat_review.rb
@@ -1,2 +1,6 @@
 class ChatReview < ApplicationRecord
+  belongs_to :user
+  belongs_to :chat
+
+  validates :score, :text, presence: true
 end
