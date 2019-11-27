3e5ade453822350ef009118d9d1b483b22bc7d24
ShoSeshake <sho122seshake@gmail.com>
2019-11-28 00:11:16 +0900

    chat_review model

---
+++ db/migrate/20191127150751_create_chat_reviews.rb
@@ -0,0 +1,11 @@
+class CreateChatReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :chat_reviews do |t|
+      t.integer :score, null: false
+      t.text :text, null: false
+      t.references :chat, null: false,index: true, foreign_key: true
+      t.references :user, null: false,index: true, foreign_key: true
+      t.timestamps
+    end
+  end
+end
