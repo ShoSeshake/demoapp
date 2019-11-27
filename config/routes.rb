941ec50172cd54bdec91c1b2f56cb8ca6d4ef82e
ShoSeshake <sho122seshake@gmail.com>
2019-11-27 17:25:22 +0900

    setting up for skyway video chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'chats#video'
+  
   resources :chats, except: [:index, :new,:create,:show,:edit,:update,:destroy] do
     collection do
       get 'video'
