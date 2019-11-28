cbd70e732b583147b65795a5e203497ed82a5ff8
ShoSeshake <sho122seshake@gmail.com>
2019-11-28 23:49:10 +0900

    blog model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_150751) do
+ActiveRecord::Schema.define(version: 2019_11_28_144043) do
+
+  create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "text", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
 
   create_table "chat_reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "score", null: false
@@ -48,6 +57,7 @@ ActiveRecord::Schema.define(version: 2019_11_27_150751) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "blogs", "users"
   add_foreign_key "chat_reviews", "chats"
   add_foreign_key "chat_reviews", "users"
   add_foreign_key "chats", "users"
