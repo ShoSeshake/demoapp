569cd2c070f891527f39955592772df3e1ced3da
ShoSeshake <sho122seshake@gmail.com>
2019-11-29 23:52:19 +0900

    yy countdown

--- app/assets/javascripts/clock.js
+++ app/assets/javascripts/clock.js
@@ -1,28 +1,30 @@
 $(function() {
-    function digitalClock() {
-        // 現在日時を取得
-        var now = new Date();
-        // 「時」を取得
-        var hour = now.getHours();
-        // 「分」を取得
-        var minute = now.getMinutes();
-        // 「秒」を取得
-        var second = now.getSeconds();
+    var elem = $("#RealtimeClock");
+    if (elem.length > 0) {
+        function digitalClock() {
+            // 現在日時を取得
+            var now = new Date();
+            // 「時」を取得
+            var hour = now.getHours();
+            // 「分」を取得
+            var minute = now.getMinutes();
+            // 「秒」を取得
+            var second = now.getSeconds();
 
-        // 0埋めで2桁表示にする　例）08:45:09
-        if (hour < 10) hour = "0" + hour;
-        if (minute < 10) minute = "0" + minute;
-        if (second < 10) second = "0" + second;
+            // 0埋めで2桁表示にする　例）08:45:09
+            if (hour < 10) hour = "0" + hour;
+            if (minute < 10) minute = "0" + minute;
+            if (second < 10) second = "0" + second;
 
-        // 現在時刻を表示する
-        var elem = document.getElementById("RealtimeClock");
-        elem.innerHTML = hour + ":" + minute + ":" + second;
+            // 現在時刻を表示する
+            elem.innerHTML = hour + ":" + minute + ":" + second;
 
-        // 500ミリ秒後に再帰呼び出し
-        setTimeout(digitalClock, 500);
-    }
+            // 500ミリ秒後に再帰呼び出し
+            setTimeout(digitalClock, 500);
+        }
 
-    window.onload = function() {
-        digitalClock();
+        window.onload = function() {
+            digitalClock();
+        }
     }
 });
