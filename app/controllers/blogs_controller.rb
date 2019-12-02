495194f3464a98428ac9e94c229e8959aeeebd85
ShoSeshake <sho122seshake@gmail.com>
2019-12-02 14:17:59 +0900

    blogs controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,62 @@
 class BlogsController < ApplicationController
+
+  before_action :set_blog, only: [:show,:edit,:update,:delete]
+  before_action :user_check, only:  [:edit,:update,:delete]
+  def index
+    
+  end
+
+  def show
+    
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blog_path(@blog)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @blog.save
+      redirect_to blog_path(@blog)
+    else
+      render :edit
+    end
+  end
+
+  def delete
+    if @blog.destroy
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+    
+  private
+  def blog_params
+    params.require(:blog).permit(
+      :title,
+      :text
+    ).merge(user_id: current_user.id)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  def user_check
+    redirect_to root_path unless current_user == @blog.user
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   before_action :header_menu
   def show
     @user = User.find(params[:id])
-    @chat = Chat.new
   end
 end
