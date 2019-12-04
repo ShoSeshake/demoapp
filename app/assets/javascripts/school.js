f5b05fb09daa07d9e1919f19b12200459df75f07
ShoSeshake <sho122seshake@gmail.com>
2019-12-04 19:21:46 +0900

    school show

---
+++ app/assets/javascripts/school.js
@@ -0,0 +1,15 @@
+$(function() {
+    var advisers = $('.school-middle__advisers__adviser');
+    advisers.on('mouseenter', function() {
+        var id = $(this).data('adviser');
+        advisers.css({
+            'opacity': '0.5'
+        })
+        $(this).css({
+            'opacity': '1'
+        })
+        var adviser = $("#adviser-description-" + id)
+        $('.adviser-introduction').hide();
+        adviser.fadeIn(500);
+    })
+})
