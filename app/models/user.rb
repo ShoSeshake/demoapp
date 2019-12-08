f5af0a0300f00eea1b4a3fa7906923912f6cfa79
ShoSeshake <sho122seshake@gmail.com>
2019-12-09 08:53:39 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    # mount_uploader :icon, ImageUploader
+    mount_uploader :icon, ImageUploader
     mount_uploader :background_image, ImageUploader
 
     has_many :chats
@@ -18,16 +18,16 @@ class User < ApplicationRecord
 
     accepts_nested_attributes_for :schedules
 
-    # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     
-    # validates :password, length: { minimum: 7, maximum: 128}
-    # validates :password_confirmation, length: { minimum: 7, maximum: 128}
-    # validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-    # validates :name, :email, :password, :password_confirmation, :profile, :icon, presence: true
-    # validates :name, :email, :profile, :icon, presence: true, on: :update, unless: :encrypted_password_changed?
-    # validates :ticket, presence: true, unless: :adviser?
-    # validates :school, presence: true, if: :adviser?
-    # validates :schedules, length: { minimum: 7, maximum: 7}, if: :adviser?
+    validates :password, length: { minimum: 7, maximum: 128}
+    validates :password_confirmation, length: { minimum: 7, maximum: 128}
+    validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+    validates :name, :email, :password, :password_confirmation, :profile, :icon, presence: true
+    validates :name, :email, :profile, :icon, presence: true, on: :update, unless: :encrypted_password_changed?
+    validates :ticket, presence: true, unless: :adviser?
+    validates :school, presence: true, if: :adviser?
+    validates :schedules, length: { minimum: 7, maximum: 7}, if: :adviser?
 
     def use_ticket
       self.update_attribute(:ticket, (self.ticket - 1))
