cf4ae9816f542316cdd3f3a032a7ed4449a288f7
ShoSeshake <sho122seshake@gmail.com>
2019-12-11 23:08:06 +0900

    search done

--- app/controllers/api/areas_controller.rb
+++ app/controllers/api/areas_controller.rb
@@ -1,16 +1,25 @@
 class Api::AreasController < ApplicationController
   def search
-
-      @q = Area.where.not(ancestry: nil).ransack(search_params)
+      areas = Area.where.not(ancestry: nil)
+      @q = areas.ransack(search_params)
       @areas = @q.result(distinct: true)
   end
 
   private
   def search_params
+    if params[:parent].present?
+      area_ids = []
+      params[:parent].each do |parent|
+        area = Area.find(parent.to_i)
+        area_ids.push(area.descendant_ids)
+        area_ids.flatten!
+      end
+    end
+    
     params.require(:q).permit(
       :sorts,
       :name_cont,
       { areas_merits_merit_id_in: [] }
-      )
+    ).merge(id_in: area_ids)
   end
 end
