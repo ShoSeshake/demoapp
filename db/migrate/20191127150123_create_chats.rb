58719fed9f3a8df0e8948249bc8bcc18b7d1ea90
ShoSeshake <sho122seshake@gmail.com>
2019-12-12 20:59:02 +0900

    migration file

--- db/migrate/20191127150123_create_chats.rb
+++ db/migrate/20191127150123_create_chats.rb
@@ -1,7 +1,6 @@
 class CreateChats < ActiveRecord::Migration[5.2]
   def change
     create_table :chats do |t|
-      t.string :adviser_peer_id
       t.string :user_peer_id
       t.datetime :start_at, null: false
       t.references :adviser, null: false,index: true, foreign_key: {to_table: :users}
